1bf76dd5b6a0d8746c89ce6f0403735a622d436e
mmik@softserveinc.com <mmik@softserveinc.com>
2021-01-18 10:09:48 +0100

    Masstransit in memory consumer weather forecast api

---
+++ Contracts/WeatherForecastUpdated.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Contracts
+{
+    public interface WeatherForecastUpdated
+    {
+        DateTime Date { get; }
+
+        int TemperatureC { get; }
+
+        int TemperatureF { get; }
+
+        string Summary { get; }
+    }
+}
---
+++ WeatherForecastApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherForecastApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IPublishEndpoint publishEndpoint)
+        {
+            _logger = logger;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            IEnumerable<WeatherForecast> data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            _publishEndpoint.Publish<WeatherForecastUpdated>(new 
+            {
+                Date = DateTime.Now,
+                TemperatureC = 12,
+                TemperatureF = 18,
+                Summary = "Rather rainy weather!"
+            });
+
+            return data;
+        }
+    }
+}
